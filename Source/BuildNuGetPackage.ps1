133338ed5350de5ae2deb76c2553578a7d4554c7
PolarbearDK <philiph@dsdevelopment.dk>
2016-07-03 09:55:26 +0200

    Bump version to 3.0.2

--- Source/BuildNuGetPackage.ps1
+++ Source/BuildNuGetPackage.ps1
@@ -7,14 +7,15 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 if($pack) {
-	& msbuild Miracle.FileZilla.Api.csproj /p:Configuration=release
+	& msbuild Miracle.FileZilla.Api/Miracle.FileZilla.Api.csproj /p:Configuration=release
 	if(!$?){throw "msbuild returned exit code $LASTEXITCODE"}
 	
-	& ..\.NuGet\NuGet.exe pack Miracle.FileZilla.Api.csproj -prop Configuration=release
+	& .NuGet\NuGet.exe pack Miracle.FileZilla.Api/Miracle.FileZilla.Api.csproj -prop Configuration=release
 	if(!$?){throw "NuGet returned exit code $LASTEXITCODE"}
 }
 
 if($push) {
 	$filename = Get-ChildItem "Miracle.FileZilla.Api.*" | Sort-Object LastWriteTime -Descending | Select -First 1
-	& ..\.NuGet\NuGet.exe push "$filename"
+	echo "Pushing $filename"
+	& .NuGet\NuGet.exe push "$filename" -Source https://www.nuget.org/api/v2/package
 }
